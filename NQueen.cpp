82846e319bb2e8a1b29497f8b2253b1c5253647f
aanand18 <50799286+aanand18@users.noreply.github.com>
2021-10-01 14:28:11 +0530

    N Queens Backtracking

---
+++ NQueen.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void printboard(bool **board,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+bool ifSafe(bool **board,int n, int row,int col){
+    for(int i=0;i<col;i++){
+        if(board[row][i]) return false;
+    }
+    for(int i=row,j=col;i>=0 &&j>=0;i--,j--){
+        if(board[i][j]) return false;
+    }
+    for(int i=row,j=col;i<n && j>=0;i++,j--){
+        if(board[i][j]) return false;
+    }
+    return true;
+}
+
+bool solve(bool **board,int n,int col){
+    if(col==n) {
+        printboard(board,n);
+        return true;
+    }
+    bool res = false;
+    for(int i=0;i<n;i++){
+        if(ifSafe(board,n,i,col)){
+            board[i][col] = 1;
+            res = solve(board,n,col+1) || res;
+            board[i][col] = 0;
+        }
+    }
+    return res;
+}
+int main(){
+    int n;
+    cin>>n;
+    bool **board;
+    board = new bool*[n];
+    for(int i = 0; i <n; i++)  board[i] = new bool[n];
+    memset(board,0,sizeof(board));
+    printboard(board,n);
+    // bool ans = solve(board,n,0);
+    // if(ans == 0) cout<<"Solution does not exist !"<<endl;
+    for(int i=0;i<n;i++) free(board[i]);
+    free(board);
+    return 0;
+}
